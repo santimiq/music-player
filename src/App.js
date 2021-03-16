0035d02d29e393f037d1507ecec9d3c787498597
Santiago Miqueo <smiqueo@hotmail.com>
2021-03-16 12:24:16 -0300

    Adding Styles to components (song and player)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
-// Importing Components
+//Import Styles
+import "./styles/app.scss"
+// Import Components
 import Player from './components/Player';
 import Song from './components/Song';
 
