866231c71e1060a981f0bd493044359902b9d257
Santiago Miqueo <smiqueo@hotmail.com>
2021-03-17 13:37:49 -0300

    Input Slider

--- src/components/Player.js
+++ src/components/Player.js
@@ -20,10 +20,15 @@ const Player = ({currentSong, isPlaying, setIsPlaying}) => {
         const current = e.target.currentTime;
         const duration = e.target.duration;
         setSongInfo({
-            ...songInfo, currentTime: current, duration: duration
+            ...songInfo, currentTime: current, duration: duration //duration can be only duration
         })
     }
-    const getTime = (time) => {
+    const dragHandler = (e) => {
+        audioRef.current.currentTime = e.target.value;
+        setSongInfo({...songInfo, currentTime: e.target.value});
+    }
+
+    const getTime = (time) => { // this function is for giving me the minutes and seconds
         return(
             Math.floor(time/60) + ":" + ("0" + Math.floor(time%60)).slice(-2)
         )
@@ -40,7 +45,12 @@ const Player = ({currentSong, isPlaying, setIsPlaying}) => {
         <div className="player">
             <div className="time-control">
                 <p>{getTime(songInfo.currentTime)}</p>
-                <input type="range" />
+                <input 
+                min={0}
+                value={songInfo.currentTime}
+                max={songInfo.duration}
+                onChange={dragHandler}
+                type="range" />
                 <p>{getTime(songInfo.duration)}</p>
             </div>
             <div className="play-control">
--- src/components/Song.js
+++ src/components/Song.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Song = ({currentSong}) => {
     return (
         <div className="song-container">
-            <img src={currentSong.cover}/>
+            <img alt={currentSong.name} src={currentSong.cover}/>
             <h1>{currentSong.name}</h1>
             <h2>{currentSong.artist}</h2>
         </div>
